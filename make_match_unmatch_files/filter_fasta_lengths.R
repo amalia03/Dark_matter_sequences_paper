1ccf31a9f5fc87e8922bacb4953793e20731619b
Amalia M. <a.mailli@yahoo.com>
2024-11-03 13:35:26 +0200

    Update filter_fasta_lengths.R

--- make_match_unmatch_files/filter_fasta_lengths.R
+++ make_match_unmatch_files/filter_fasta_lengths.R
@@ -1,27 +1,26 @@
 #!/bin/R
 ## This script takes a fasta file as an input and only outputs sequences of a specific length. 
+## Edit: The original version tabulated the sequences next to the id instead of below it, which meant I created an extra file and script to untabulate the files. 
 
-read.fa <- function(f){
-    l <- readLines( f )
-    id <- l[seq(1, length(l), 2)]
-    seq <- l[seq(2, length(l), 2)]
-    names(seq) <- id
-    seq
-}
+#Import fasta
+miseq.fa <- readLines("all_sequences_Illumina_Miseq.fasta")
 
-assembled.fa <- read.fa("all_merged.fa")
-read.l <- nchar(assembled.fa)
+#Seperate id and sequence lines (assuming the sequence is not formatted into multiple lines
+seqs <- miseq.fa[seq(2,length(miseq.fa), 2)]
+ids <- miseq.fa[seq(1,length(miseq.fa)-1, 2)]
 
-plot(sort(read.l), type="l")
-table.length <- table(read.l)
+##Make a variable with the length of the sequences
+read.l <- nchar(seqs)
+###
+Processing time, in this case, keep sequences with length larger than 300bp.
+seqs.new <- seqs[read.l>300]
+ids.new <- ids[read.l>300]
 
-l <- length(read.l[read.l >= 300])
-r.300 <- read.l[read.l >= 300]
-plot(sort(r.300), type="l", ylab="Sequence length (bp)")
-abline(h=mean(r.300), col="darkgrey", lty=2)
-text(100000, 375, "mean")
-
-assembled.300 <- assembled.fa[read.l>=300]
-head(cbind(assembled.300, names(assembled.300)))
-
-write.table(assembled.300,"assembled_300.tsv", row.names=T, quote=F, sep="\t", col.names=F)
+#Make a new variable that in the fasta format
+new.fa <- capture.output(for (i in 1:length(seqs.new)) {
+                             cat(ids.new[i], "\n")  # Print each element from A
+                             cat(seqs.new[i], "\n")  # Print each element from B
+                         })
+#Output new fasta
+write.table(new.fa
+          , "all_sequences_Illumina_Miseq_mod.fasta", row.names=F, quote=F, col.names=F)
